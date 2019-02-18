44b90e6fcd6718c8fd105f62b81cfdeadd79c470
Calios <calios_1124@163.com>
2019-02-18 10:53:28 +0800

    reconstruct, add out_of_range test in 2.array

---
+++ 2.array/out_of_range.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {    
+    int i = 0;
+    int arr[3] = {0};
+    for (; i <= 3; i++) {
+        arr[i] = 0;
+        printf("hello world\n");
+    }
+    return 0;
+}
